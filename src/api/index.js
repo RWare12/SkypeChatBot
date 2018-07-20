25f84a28069c5197f7d8b6375b455e9fde474a5a
kinghoratiq <king_horatiq69@yahoo.com>
2018-07-20 11:09:28 +0800

    added proactive message implementation

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,15 @@
 import { version } from '../../package.json';
 import { Router } from 'express';
 import facets from './facets';
+var restify = require('restify');
+var builder = require('botbuilder');
+
+var connector = new builder.ChatConnector({
+	  appId: '14c384d2-831f-404c-847f-803879cffc5c',
+	  appPassword: 'hrdLVQT4628=emhmOJK0]^%'
+});
+
+var bot = new builder.UniversalBot(connector);
 
 export default ({ config, db }) => {
 	let api = Router();
@@ -10,7 +19,18 @@ export default ({ config, db }) => {
 
 	api.post('/SkypeChatbot', (req, res) => {
 		switch (req.body.queryResult.action) {
-			case 'test.test':
+			case 'proactive.message':
+				let channelId = 'skype';
+				let id = req.body.originalDetectIntentRequest.payload.address.id;
+				var user = req.body.originalDetectIntentRequest.payload.address.user;
+				let conversation = req.body.originalDetectIntentRequest.payload.address.conversation;
+				let botDetails = req.body.originalDetectIntentRequest.payload.address.bot;
+				let serviceUrl = req.body.originalDetectIntentRequest.payload.address.serviceUrl;
+				let address = {id, channelId, user, conversation, botDetails, serviceUrl};
+				var msg = new builder.Message().address(address);
+				msg.text('pushed message');
+				msg.textLocale('en-US');
+				bot.send(msg);
 				return res.json({'fulfillmentText':'test output'});
 				break;
 			case 'dog.get':
@@ -31,8 +51,9 @@ export default ({ config, db }) => {
 						'quickReplies': {
 							'title': 'Here are some things you can do with this bot:',
 							'quickReplies': [
-								'Tell me a joke',
-								'dog'
+								'tell me a joke',
+								'dog',
+								'push'
 							]
 						},
 						'platform': 'SKYPE'
